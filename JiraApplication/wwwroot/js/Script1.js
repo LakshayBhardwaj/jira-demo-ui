e5229e3dc53099aab2f02cd67e30576c0195bbdb
bhardwaj.lakshay <bhardwaj.lakshay@thinksys.com>
2019-05-17 18:37:22 +0530

    inital commit

---
+++ JiraApplication/wwwroot/js/Script1.js
@@ -0,0 +1,18 @@
+﻿function runApi(x) {
+    $.ajax({
+        url: "http://localhost:53819/api/values/list1",
+
+
+        type: "POST",
+        data: {
+            getFilter: x
+            //Data:"Hello there"
+        },
+
+        success: function (response) { console.log(response); },
+        error: function (response) { console.log(response); debugger; },
+        failure: function (response) { debugger; }
+    });
+}
+
+runApi('');
---
+++ JiraApplication/wwwroot/js/dropDown.js
@@ -0,0 +1,90 @@
+﻿var checkboxArray = new Array();
+$(document).click(function (e) {
+    var container = $("#p12");
+    var projects = "";
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        document.getElementById("myDropdown").classList.remove("show");
+    }
+
+    if (e.target.classList == "link") {
+        //  alert($(e.target).attr('classList'));
+        checkbox = document.getElementById(e.toElement.id).previousSibling;
+        if ($("#" + checkbox.id).prop('checked') == true) {
+            $("#" + checkbox.id).prop("checked", false);
+            var rm = checkboxArray.indexOf(checkbox.value);
+            checkboxArray.splice(rm, 1);
+        } else {
+            $("#" + checkbox.id).prop("checked", true);
+            checkboxArray.push(checkbox.value);
+        }
+
+        for (var i = 0; i < checkboxArray.length; i++) {
+
+            if (i == 0) {
+                projects = projects + checkboxArray[i];
+            } else {
+                projects = projects + ",+" + checkboxArray[i];
+            }
+        }
+
+
+        runApi('project +in +(' + projects + ')');
+
+    }
+
+    if (e.target.classList == "project") {
+        //  alert($(e.target).attr('classList'));
+        checkbox = e.toElement;
+        //console.log(checkbox)
+        if ($("#" + checkbox.id).prop('checked') == true) {
+
+            checkboxArray.push(checkbox.value);
+        } else {
+            var rm = checkboxArray.indexOf(checkbox.value);
+            checkboxArray.splice(rm, 1);
+
+        }
+        console.log(checkboxArray)
+        for (var i = 0; i < checkboxArray.length; i++) {
+            console.log(checkboxArray.length);
+            if (i == 0) {
+                projects = projects + checkboxArray[i];
+            } else {
+                projects = projects + ",+" + checkboxArray[i];
+            }
+        }
+
+        console.log(projects);
+        runApi('project +in +(' + projects + ')');
+
+    }
+
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.add("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i, checkbox1;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    checkbox1 = div.getElementsByTagName("input");
+
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+            checkbox1[i + 1].style.display = "";
+        } else {
+            a[i].style.display = "none";
+            checkbox1[i + 1].style.display = "none";
+        }
+    }
+}
+
